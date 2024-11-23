546b9d8c157583a23c93ea98bff033a1a540cb13
Javier Fernandez <javierjobing@gmail.com>
2024-11-23 12:05:11 +0100

    realizado ejrcicio.

--- src/main/java/com/campusdual/classroom/Exercise06.java
+++ src/main/java/com/campusdual/classroom/Exercise06.java
@@ -1,5 +1,6 @@
 package com.campusdual.classroom;
 
+import java.sql.SQLOutput;
 import java.util.Calendar;
 
 public class Exercise06 {
@@ -9,9 +10,12 @@ public class Exercise06 {
         Calendar now = Calendar.getInstance();
         int month = now.get(Calendar.MONTH) + 1;
         int hour = now.get(Calendar.HOUR_OF_DAY);
+        checkHour(21);
+        checkSeason(6);
+
     }
 
-    //TODO ↓
+
     // Si la hora es desde las 0:00 a las 6:59, imprimir por pantalla → It's night
     // Si la hora es desde las 7:00 a las 13:59, imprimir por pantalla → It's morning
     // Si la hora es desde las 14:00 a las 20:59, imprimir por pantalla → It's afternoon
@@ -20,44 +24,84 @@ public class Exercise06 {
         {
             switch (hour) {
                 case 21:
+                    System.out.println("It's night");
+                    break;
                 case 22:
+                    System.out.println("It's night");
+                    break;
                 case 23:
+                    System.out.println("It's night");
+                    break;
                 case 0:
+                    System.out.println("It's night");
+                    break;
                 case 1:
+                    System.out.println("It's night");
+                    break;
                 case 2:
+                    System.out.println("It's night");
+                    break;
                 case 3:
+                    System.out.println("It's night");
+                    break;
                 case 4:
+                    System.out.println("It's night");
+                    break;
                 case 5:
-                case 6: {
-
-                }
+                    System.out.println("It's night");
+                    break;
+                case 6:
+                    System.out.println("It's night");
+                    break;
                 case 7:
+                    System.out.println("It's morning");
+                    break;
                 case 8:
+                    System.out.println("It's morning");
+                    break;
                 case 9:
+                    System.out.println("It's morning");
+                    break;
                 case 10:
+                    System.out.println("It's morning");
+                    break;
                 case 11:
+                    System.out.println("It's morning");
+                    break;
                 case 12:
-                case 13: {
-
-
-                }
+                    System.out.println("It's morning");
+                    break;
+                case 13:
+                    System.out.println("It's morning");
+                    break;
                 case 14:
+                    System.out.println("It's afternoon");
+                    break;
                 case 15:
+                    System.out.println("It's afternoon");
+                    break;
                 case 16:
+                    System.out.println("It's afternoon");
+                    break;
                 case 17:
+                    System.out.println("It's afternoon");
+                    break;
                 case 18:
+                    System.out.println("It's afternoon");
+                    break;
                 case 19:
-                case 20: {
-
-
-                }
+                    System.out.println("It's afternoon");
+                    break;
+                case 20:
+                    System.out.println("It's afternoon");
+                    break;
                 default:
                     throw new IllegalArgumentException("Unexpected value: " + hour);
             }
         }
     }
 
-    //TODO ↓
+
     // Si es Enero, Febrero o Marzo, imprimir por pantalla → Winter
     // Si es Abril, Mayo o Junio, imprimir por pantalla → Spring
     // Si es Julio, Agosto o Septiembre, imprimir por pantalla → Summer
@@ -65,29 +109,41 @@ public class Exercise06 {
     public static void checkSeason(int month) {
         switch (month) {
             case 1:
+                System.out.println("Winter");
+                break;
             case 2:
-            case 3: {
-
-
-            }
+                System.out.println("Winter");
+                break;
+            case 3:
+                System.out.println("Winter");
+                break;
             case 4:
+                System.out.println("Spring");
+                break;
             case 5:
-            case 6: {
-
-
-            }
+                System.out.println("Spring");
+                break;
+            case 6:
+                System.out.println("Spring");
+                break;
             case 7:
+                System.out.println("Summer");
+                break;
             case 8:
-            case 9: {
-
-
-            }
+                System.out.println("Summer");
+                break;
+            case 9:
+                System.out.println("Summer");
+                break;
             case 10:
+                System.out.println("Autumn");
+                break;
             case 11:
-            case 12: {
-
-
-            }
+                System.out.println("Autumn");
+                break;
+            case 12:
+                System.out.println("Autumn");
+                break;
             default:
                 throw new IllegalArgumentException("Unexpected value: " + month);
         }
